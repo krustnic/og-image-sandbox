127c5724930c1264c5dbb83cea6ce870be7b30fe
krustnic <krustnic@yandex-team.ru>
2022-10-07 16:16:08 +0300

    arenda url

--- app/index.js
+++ app/index.js
@@ -44,7 +44,7 @@ app.get('/page', (req, res) => {
     if (req.query.type === '2') {
         content = getContent('http://og.repo.su/page', 'https://arenda.yandex.ru/og-image/?type=landing-calculator-arenda&address=%D0%A0%D0%BE%D1%81%D1%81%D0%B8%D1%8F%2C+%D0%A1%D0%B0%D0%BD%D0%BA%D1%82-%D0%9F%D0%B5%D1%82%D0%B5%D1%80%D0%B1%D1%83%D1%80%D0%B3%2C+%D0%9A%D1%83%D1%88%D0%B5%D0%BB%D0%B5%D0%B2%D1%81%D0%BA%D0%B0%D1%8F+%D0%B4%D0%BE%D1%80%D0%BE%D0%B3%D0%B0&numberOfRooms=0&area=64&floor=6&renovation=RENOVATION_DESIGNER_RENOVATION');
     } else {
-        content = getContent('http://og.repo.su/page', 'http://og.repo.su/og?type=1&amp;temp=1&amp;v=1');
+        content = getContent('http://og.repo.su/page', 'https://arenda.yandex.ru/og-image/?type=landing-calculator-arenda&address=%D0%A0%D0%BE%D1%81%D1%81%D0%B8%D1%8F%2C+%D0%A1%D0%B0%D0%BD%D0%BA%D1%82-%D0%9F%D0%B5%D1%82%D0%B5%D1%80%D0%B1%D1%83%D1%80%D0%B3%2C+%D0%9A%D1%83%D1%88%D0%B5%D0%BB%D0%B5%D0%B2%D1%81%D0%BA%D0%B0%D1%8F+%D0%B4%D0%BE%D1%80%D0%BE%D0%B3%D0%B0&numberOfRooms=4&area=200&floor=6&renovation=RENOVATION_DESIGNER_RENOVATION');
     }
     
     res.send(content);
