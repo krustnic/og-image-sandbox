e4a7b300ec3a9f33567049b9b6d0cbd57709acb7
krustnic <krustnic@yandex-team.ru>
2022-10-09 11:10:34 +0300

    canonical diff

--- app/index.js
+++ app/index.js
@@ -6,12 +6,12 @@ const express = require('express')
 const app = express()
 const port = 80
 
-const getContent = (pageName, ogImagePath) => {
+const getContent = (pageName, canonical, ogImagePath) => {
     return`
 <html>
 <head>
     <title>Title</title>
-    <link rel="canonical" href="${pageName}">
+    <link rel="canonical" href="${canonical}">
     <meta property="og:url" content="${pageName}">
     <meta property="og:title" content="og image server">
     <meta property="og:description" content="Owner">
@@ -43,9 +43,15 @@ app.get('/page', (req, res) => {
     let content;
     
     if (req.query.type === '2') {
-        content = getContent('http://og.repo.su/page', 'https://arenda.yandex.ru/og-image/?type=landing-calculator-arenda&address=%D0%A0%D0%BE%D1%81%D1%81%D0%B8%D1%8F%2C+%D0%A1%D0%B0%D0%BD%D0%BA%D1%82-%D0%9F%D0%B5%D1%82%D0%B5%D1%80%D0%B1%D1%83%D1%80%D0%B3%2C+%D0%9A%D1%83%D1%88%D0%B5%D0%BB%D0%B5%D0%B2%D1%81%D0%BA%D0%B0%D1%8F+%D0%B4%D0%BE%D1%80%D0%BE%D0%B3%D0%B0&numberOfRooms=0&area=64&floor=6&renovation=RENOVATION_DESIGNER_RENOVATION');
+        content = getContent(
+            'http://og.repo.su/page?type=2',
+            'http://og.repo.su/page',
+            'https://arenda.yandex.ru/og-image/?type=landing-calculator-arenda&address=%D0%A0%D0%BE%D1%81%D1%81%D0%B8%D1%8F%2C+%D0%A1%D0%B0%D0%BD%D0%BA%D1%82-%D0%9F%D0%B5%D1%82%D0%B5%D1%80%D0%B1%D1%83%D1%80%D0%B3%2C+%D0%9A%D1%83%D1%88%D0%B5%D0%BB%D0%B5%D0%B2%D1%81%D0%BA%D0%B0%D1%8F+%D0%B4%D0%BE%D1%80%D0%BE%D0%B3%D0%B0&numberOfRooms=0&area=64&floor=6&renovation=RENOVATION_DESIGNER_RENOVATION');
     } else {
-        content = getContent('http://og.repo.su/page', 'https://arenda.yandex.ru/og-image/?type=landing-calculator-arenda&address=%D0%A0%D0%BE%D1%81%D1%81%D0%B8%D1%8F%2C+%D0%A1%D0%B0%D0%BD%D0%BA%D1%82-%D0%9F%D0%B5%D1%82%D0%B5%D1%80%D0%B1%D1%83%D1%80%D0%B3%2C+%D0%9A%D1%83%D1%88%D0%B5%D0%BB%D0%B5%D0%B2%D1%81%D0%BA%D0%B0%D1%8F+%D0%B4%D0%BE%D1%80%D0%BE%D0%B3%D0%B0&numberOfRooms=4&area=200&floor=6&renovation=RENOVATION_DESIGNER_RENOVATION');
+        content = getContent(
+            'http://og.repo.su/page?type=1',
+            'http://og.repo.su/page',
+            'https://arenda.yandex.ru/og-image/?type=landing-calculator-arenda&address=%D0%A0%D0%BE%D1%81%D1%81%D0%B8%D1%8F%2C+%D0%A1%D0%B0%D0%BD%D0%BA%D1%82-%D0%9F%D0%B5%D1%82%D0%B5%D1%80%D0%B1%D1%83%D1%80%D0%B3%2C+%D0%9A%D1%83%D1%88%D0%B5%D0%BB%D0%B5%D0%B2%D1%81%D0%BA%D0%B0%D1%8F+%D0%B4%D0%BE%D1%80%D0%BE%D0%B3%D0%B0&numberOfRooms=4&area=200&floor=6&renovation=RENOVATION_DESIGNER_RENOVATION');
     }
     
     res.send(content);
